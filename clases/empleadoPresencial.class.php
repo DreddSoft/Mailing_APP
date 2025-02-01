d1f12d81572fd22683feaa732019e2c40e6d40d3
Ivan-Trevi <ilopjim3107@g.educaand.es>
2025-02-01 14:08:02 +0100

    Código comentado

--- clases/empleadoPresencial.class.php
+++ clases/empleadoPresencial.class.php
@@ -7,10 +7,10 @@
 // - Metodo mostrarDatos(): el to string que muestre todos los datos del empleado
 // - GETTERS y SETTERS
 
-// Incluimos la base de datos
-require_once($_SERVER['DOCUMENT_ROOT'] . '/Mailing_APP/bd.class.php');
+// Incluye nuestra base de datos que vamos a implementar
+require_once($_SERVER['DOCUMENT_ROOT'] . '/Mailing_APP/bd.class.php'); // Incluye el archivo bd.class.php que contiene la clase bd para la conexión y manejo de la base de datos.
 
-require_once("empleado.class.php");
+require_once("empleado.class.php"); // Incluye el archivo empleado.class.php que contiene la definicion de la clase empleado.
 
 class EmpleadoPresencial extends Empleado
 {
@@ -22,46 +22,52 @@ class EmpleadoPresencial extends Empleado
     // Constructor
     public function __construct($nombre, $edad, $salario, $idDpto, $oficina, $id = null)
     {
+        //Llama al constructor de la clase padre (empleado) para inicializar los atributos heredados.
         parent::__construct($nombre, $edad, $salario, $idDpto);
         $this->oficina = $oficina;
 
-        if ($id === null) {
+        if ($id === null) { //Si no se pasa un id, crea un nuevo empleado en la base de datos.
             try {
-                $bd = new bd();
+                $bd = new bd(); // Crea una instancia de la clase bd.
 
                 // Conectamos a la base de datos
                 $bd->conectar();
-
+                // Sentencia SQL para insertar un nuevo empleado en la base de datos.
                 $sql = "INSERT INTO empleados (nombre, edad, salario, oficina, horasConexion, rango, idDpto)
             VALUES ('$nombre', $edad, $salario, '$oficina', NULL, 0, $idDpto)";
 
-                $update = $bd->actualizarDatos($sql);
+                $update = $bd->actualizarDatos($sql); // Ejecuta la sentencia SQL-
 
                 if (!$update) {
+                    // Si la inserción no se realiza correctamente, lanza una excepción
                     throw new Exception();
                 }
             } catch (Exception $e) {
                 throw new Exception("No se ha podido crear el EMPLEADO TIPO ENCARGADO");
             } finally {
+                // Cierra la conexión a la base de datos
                 $bd->cerrar();
             }
         } else {
+            // Si se pasa un id, lo asigna al atributo id.
             $this->id = $id;
         }
     }
 
-    // GETTER Y SETTER propios
+    // GETTER Y SETTER
+
+    // GETTER para el atributo oficina
     public function getOficina()
     {
         return $this->oficina;
     }
-
+    // SETTER para el atributo oficina
     public function setOficina($oficina)
     {
         $this->oficina = $oficina;
     }
 
-    // Funcion de la clase empleado, plimorfismo porque añade mas detalles
+    // Método que muestra los detalles del empleado, aplica polimorfismo añadiendo el atributo oficina.
     public function mostrarDetalles()
     {
         return parent::mostrarDetalles() . " , oficina: $this->oficina";
